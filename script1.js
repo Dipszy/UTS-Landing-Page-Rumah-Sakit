a1f07476cf89aa23a2b62024438f3921231b12c8
mhmaddiqbll <119943243+Dipszy@users.noreply.github.com>
2024-11-14 21:19:21 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,3 @@
-// Menambahkan efek animasi saat halaman dimuat
 window.onload = function() {
     const profileSection = document.querySelector('.company-profile');
     profileSection.classList.add('fade-in');
